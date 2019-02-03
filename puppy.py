1250d86e7107941cfed7cc8ce7a45070cb31675f
Kash <kshampur@gmail.com>
2019-02-03 14:27:47 -0500

    Add files via upload

---
+++ puppy.py
@@ -0,0 +1,21 @@
+#puppy
+
+class puppy:
+
+    def __init__(self):
+        print('what does a good boy do?')
+
+    def bork(self):
+        print('bork')
+        #TODO add web api to launch browser to borking videos
+
+
+
+if __name__ == "__main__":
+    new_pup = puppy()
+
+    new_pup.bork()
+
+
+    
+
